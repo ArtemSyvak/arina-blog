77a73618f524e3ecabaa1654fed411f2462012da
Artem Syvak <art.syvak@gmail.com>
2020-04-12 18:23:11 +0300

    - added react-bootstrap, node-sass

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+
+        <Button 
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          >Learn React
+        </Button>
       </header>
     </div>
   );
