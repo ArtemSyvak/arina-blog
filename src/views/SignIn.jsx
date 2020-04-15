22c2f6eed0d6f3dc0e766c18078c85bc790acaa2
Artem Syvak <art.syvak@gmail.com>
2020-04-15 03:18:07 +0300

    - email/password & google authorization

---
+++ src/views/SignIn.jsx
@@ -0,0 +1,62 @@
+import React, { useCallback, useContext } from "react";
+import { withRouter, Redirect } from "react-router";
+import app from "../firebase";
+import {signInWithGoogle} from "../firebase";
+import { AuthContext } from "../providers/Auth";
+
+const Login = ({ history }) => {
+  const handleLogin = useCallback(
+    async event => {
+      event.preventDefault();
+      const { email, password } = event.target.elements;
+      try {
+        await app
+          .auth()
+          .signInWithEmailAndPassword(email.value, password.value);
+        history.push("/");
+      } catch (error) {
+        alert(error);
+      }
+    },
+    [history]
+  );
+
+  const handleGoogleAuth = useCallback(
+    async event => {
+      event.preventDefault();
+      try {
+        signInWithGoogle();
+        history.push("/");
+      } catch (error) {
+        alert(error);
+      }
+    },
+    [history]
+  );
+
+  const { currentUser } = useContext(AuthContext);
+
+  if (currentUser) {
+    return <Redirect to="/" />;
+  }
+
+  return (
+    <div>
+      <h1>Log in</h1>
+      <form onSubmit={handleLogin}>
+        <label>
+          Email
+          <input name="email" type="email" placeholder="Email" />
+        </label>
+        <label>
+          Password
+          <input name="password" type="password" placeholder="Password" />
+        </label>
+        <button type="submit">Log in</button>
+      </form>
+      <button onClick={handleGoogleAuth}>Sign In by Google</button>
+    </div>
+  );
+};
+
+export default withRouter(Login);
